b1f70595626ec6496d26e36a63b0249a9f1c27e2
Jenn <jenn.a.martinez@outlook.com>
2019-09-06 16:28:22 -0700

    Added counters.jsx file and added multiple counter tags.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,15 +10,16 @@ class Counter extends Component {
     //     this.handleIncrement = this.handleIncrement.bind(this);
     // }
 
-    handleIncrement = () => {
-        this.setState({count: this.state.count + 1})
+    handleIncrement = product => {
+        console.log(product);
+        this.setState({count: this.state.count + 1});
     };
 
     render() { 
         return (
         <div>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+            <button onClick={() => this.handleIncrement(product)} className="btn btn-secondary btn-sm">Increment</button>
         </div>
         );
     }
