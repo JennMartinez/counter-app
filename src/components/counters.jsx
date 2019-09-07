64d887624b2c940db78447402260371c0ce20732
Jenn <jenn.a.martinez@outlook.com>
2019-09-06 17:23:16 -0700

    Added a button for deleting and a handleDelete function.

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,11 +10,16 @@ class Counters extends Component {
             { id: 4, value: 0 }
         ]
     };
+
+    handleDelete = () => {
+        console.log("Hey, it is working!");
+    }
+
     render() {
         return (
             <div>
                 {this.state.counters.map(counter => (
-                <Counter key={counter.id} value={counter.value} id={counter.id}>
+                <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} id={counter.id}>
                 </Counter>
                 ))}
             </div>
